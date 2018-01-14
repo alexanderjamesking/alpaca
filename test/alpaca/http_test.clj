704b657bb310b02a9cca304ca50b218a3a74738b
Alex King <alex@alexanderjamesking.com>
2018-01-14 21:08:41 +0000

    http kit and core async - quickest response

--- test/alpaca/http_test.clj
+++ test/alpaca/http_test.clj
@@ -1,7 +1,8 @@
 (ns alpaca.http-test
   (:require  [clojure.test :refer [deftest testing is use-fixtures]]
              [clojure.data.json :as json]
-             [org.httpkit.client :as http])
+             [org.httpkit.client :as http]
+             [clojure.core.async :as async :refer [<! >! go >!! <!! chan timeout mult tap close!]])
   (:import com.github.tomakehurst.wiremock.WireMockServer))
 
 (def wiremock-url "http://localhost:8080")
@@ -23,11 +24,32 @@
   @(http/post (str wiremock-url "/__admin/mappings")
               {:body (json/write-str options)}))
 
+(defn stub-text
+  ([url text]
+   (stub-text url text 0))
+  ([url text delay-ms]
+   (stub {:request {:method "GET"
+                    :url url}
+          :response {:status 200
+                     :fixedDelayMilliseconds delay-ms
+                     :body text
+                     :headers {:Content-Type "text/plain"}}})))
+
 (deftest hello-wiremock-test
-  (stub {:request {:method "GET"
-                   :url "/"}
-         :response {:status 200
-                    :fixedDelayMilliseconds 100
-                    :body "Hello World!"
-                    :headers {:Content-Type "text/plain"}}})
+  (stub-text "/" "Hello World!")
   (is (= "Hello World!" (-> (http/get wiremock-url) deref :body))))
+
+(deftest return-quickest-of-two-endpoints
+  (stub-text "/a" "Response from A!" 200)
+  (stub-text "/b" "Response from B!" 400)
+
+  (let [a (chan)
+        b (chan)]
+    (http/get (str wiremock-url "/a") (fn [res]
+                                        (go (>! a (:body res)))))
+    (http/get (str wiremock-url "/b") (fn [res]
+                                        (go (>! b (:body res)))))
+
+    (is (= "Response from A!" (first (async/alts!! [a b]))))
+    ;; to ensure we consume the result of b to end the test cleanly
+    (is (= "Response from B!" (<!! b)))))
